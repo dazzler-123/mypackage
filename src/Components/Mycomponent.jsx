ac2997dab11153587e2ee7db715fb86da6290818
chandan <chandan.maurya@ebix.com>
2024-07-09 12:40:01 +0530

    Initial commit

--- src/Components/Mycomponent.jsx
+++ src/Components/Mycomponent.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import "./MyComponent.css"
 const  Mycomponent=() =>{
   return (
     <div className="my-component">Mycomponent</div>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+import '../src/Components/style.css'
 export  * from '../src/Components';
 // export { default as Mycomponent } from '../src/Components/Mycomponent';
