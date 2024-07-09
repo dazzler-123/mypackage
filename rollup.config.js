ad2a461153c864265a7be4c6d6ab89fa941f5235
chandan <chandan.maurya@ebix.com>
2024-07-09 12:07:42 +0530

    Initial commit

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
@@ -29,6 +30,10 @@ export default {
         presets: ['@babel/preset-env', '@babel/preset-react'],
     }),
     commonjs(),
+    postcss({
+        extract: true,
+        minimize: true,
+      }),
     terser(),
   ],
 };
---
+++ src/Components/Mycomponent.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./MyComponent.css"
+const  Mycomponent=() =>{
+  return (
+    <div className="my-component">Mycomponent</div>
+  )
+}
+export default Mycomponent
