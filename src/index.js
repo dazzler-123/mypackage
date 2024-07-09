50830c216906e295c1048b35af1c57433a434126
chandan <chandan.maurya@ebix.com>
2024-07-09 11:57:23 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-// export  * from '../src/Components';
-export { default as Mycomponent } from '../src/Components/Mycomponent';
+export  * from '../src/Components';
+// export { default as Mycomponent } from '../src/Components/Mycomponent';
