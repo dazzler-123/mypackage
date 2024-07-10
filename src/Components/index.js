6579679447b78050cbd2d5a6b58289966570ae48
chandan <chandan.maurya@ebix.com>
2024-07-10 16:11:27 +0530

    fixed

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,6 @@
-export { default as MyCheckBox  } from './CheckBox';
-export { default as MyDate  } from './Date';
-export { default as MyDropDown  } from './DropDown';
-export { default as MyRadioGroup  } from './RadioGroup';
-export { default as MyMultiSelectDropDown  } from './MultiSelectDropDown';
+// export { default as MyCheckBox  } from './CheckBox';
+// export { default as MyDate  } from './Date';
+// export { default as MyDropDown  } from './DropDown';
+// export { default as MyRadioGroup  } from './RadioGroup';
+// export { default as MyMultiSelectDropDown  } from './MultiSelectDropDown';
+export { default as Mycomponent  } from './Mycomponent';
