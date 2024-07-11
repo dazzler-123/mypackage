4a13d535ebb1a77e9dfa352f1bf24bdb98c1ea31
chandan <chandan.maurya@ebix.com>
2024-07-11 15:54:46 +0530

    fixed

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,4 @@
 export { default as MyInputTypeCheckBox  } from './PureComponent/MyInputTypeCheckBox';
-// export { default as MyDate  } from './Date';
 export { default as MyInputTypeDropDown  } from './PureComponent/MyInputTypeDropDown';
 export { default as MyInputTypeRadioGroup  } from './PureComponent/MyInputTypeRadioGroup';
+export { default as MyInputTypeText  } from './PureComponent/MyInputTypeText';
