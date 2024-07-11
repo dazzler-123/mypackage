6746595c7f2b7dd62fb20ebb865749b4c28f6043
chandan <chandan.maurya@ebix.com>
2024-07-11 15:55:04 +0530

    fixed

---
+++ src/Components/PureComponent/MyInputTypeText.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { FormControl } from "@mui/material";
+import TextField from "@mui/material/TextField";
+const MyInputTypeText = ({
+  multilanguage,
+  allprops,
+  TextFieldLabel,
+  placeholder,
+  error,
+  helperText,
+  autoComplete = "off",
+  type = "text",
+  onChange,
+  name="",
+  sx={}
+}) => {
+  
+  return (
+    <FormControl sx={{ width: "100%" }}>
+      <TextField
+        label={multilanguage(TextFieldLabel)}
+        type={type}
+        name={name}
+        autoComplete={autoComplete}
+        placeholder={multilanguage(placeholder)}
+        error={error}
+        helperText={helperText}
+        {...allprops}
+        onChange={onChange}
+        sx={sx}
+        fullWidth
+      />
+    </FormControl>
+  );
+};
+
+export default MyInputTypeText;
